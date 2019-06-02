9651fff2eb3b3c9c61b01bdd1ba9f61895e58953
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-06-02 17:32:58 -0600

    setup vue router with vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,14 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+    <v-toolbar app compact>
+      <v-toolbar-title class="headline">Radio Net Node</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-btn to="/" flat>Home</v-btn>
+      <v-btn to="/about" flat>About</v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
