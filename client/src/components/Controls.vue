443fa3cf76e2aafeb0c74be4cbf091b7c37c1e91
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-06-02 19:27:08 -0600

    convert functions to typescript

--- client/src/components/Controls.vue
+++ client/src/components/Controls.vue
@@ -20,25 +20,25 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { pause, restart, next, stop, clear, displayCurrent, playQueue, loadFile, songsByType } from "./lib/radio-dj.js";
+import { pause, restart, next, stop, clear, displayCurrent, playQueue, loadFile, songsByType } from "./lib/radio-dj";
 
 @Component({})
 export default class Controls extends Vue {
     mounted() {
         // get current track and display
-        displayCurrent('#current_info_string');
+        displayCurrent('#current_info_string', () => {});
 
         // update current song title and queue every n seconds
         var interval = 1000;
         setInterval(this.updateDisplay, interval);
 
         // get streams and draw select
-        songsByType(5, function (resp: any, stat: any){
-            console.log(resp);
-            // $.each(resp, function( index, value ) {
-            //     $('#stream_selector').append('<option value="' + value.ID + '">' + value.title + '</option>');
-            // });
-        });
+        // songsByType(5, function (resp: any, stat: any){
+        //     console.log(resp);
+        //     // $.each(resp, function( index, value ) {
+        //     //     $('#stream_selector').append('<option value="' + value.ID + '">' + value.title + '</option>');
+        //     // });
+        // });
     };
 
     loadStream() {
--- client/src/components/lib/radio-dj.ts
+++ client/src/components/lib/radio-dj.ts
@@ -1,131 +1,128 @@
 // default empty callback
-function c() { }
+function c() {}
 
 // titles of event run types
 var event_run_types = ['No Repeat', 'Repeat by Day', 'Repeat by Day and Hour', 'Manual Event', 'Start-up Event'];
 
 // --- Control Functions ---
 
-function sendControl(command) {
-    var arg = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
-    var callback = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : c;
+function sendControl(command: String, arg: any, callback: Function) {
+    arg = arg !== undefined ? arg : '';
+    callback = callback !== undefined ? callback : c;
 
     // console.log(command, arg);
-    $.get("/opt", { command: command, arg: arg }, callback);
+    //$.get("/opt", { command: command, arg: arg }, callback);
 }
 
-export function loadFile(id) {
+export function loadFile(id: Number) {
     sendControl('LoadTrackToTop', id, next);
 }
 
 // simple control functions
 export function restart() {
-    sendControl('RestartPlayer');
+    sendControl('RestartPlayer', '', c);
 }
 export function clear() {
-    sendControl('ClearPlaylist');
+    sendControl('ClearPlaylist', '', c);
 }
 export function fromIntro() {
-    sendControl('PlayFromIntro');
+    sendControl('PlayFromIntro', '', c);
 }
 export function next() {
-    sendControl('PlayPlaylistTrack');
+    sendControl('PlayPlaylistTrack', '', c);
 }
 export function stop() {
-    sendControl('StopPlayer');
+    sendControl('StopPlayer', '', c);
 }
-export function pause(state) {
-    sendControl('PausePlayer', state);
+export function pause(state: Number) {
+    sendControl('PausePlayer', state, c);
 }
 
 // --- Queue + Current Functions ---
 
-export function displayCurrent(divID) {
+export function displayCurrent(divID: String, callback: any) {
     var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : c;
 
-    currentTrack(function (resp, stat) {
-        var i = resp[0];
-        var info_string = (i === undefined ? "" : i.title + " - " + i.artist);
-        $(divID).text(info_string);
-        callback();
-    });
+    // currentTrack(function (resp: any, stat: any) {
+    //     var i = resp[0];
+    //     var info_string = (i === undefined ? "" : i.title + " - " + i.artist);
+    //     //$(divID).text(info_string);
+    //     callback();
+    // });
 }
 
 // get song info by ID
-export function songByID(id) {
+export function songByID(id: Number, callback: any) {
     var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : c;
 
-    $.get("/query?q=song_id&arg=" + id, callback);
+    //$.get("/query?q=song_id&arg=" + id, callback);
 }
 
 // get song info by type
-export function songsByType(type) {
+export function songsByType(type: String, callback: any) {
     var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : c;
 
-    $.get("/query?q=song_type&arg=" + type, callback);
+    //$.get("/query?q=song_type&arg=" + type, callback);
 }
 
 // get song info by subcat
-export function songsBySubcat(subcat) {
+export function songsBySubcat(subcat: String, callback: any) {
     var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : c;
 
-    $.get("/query?q=song_subcat&arg=" + subcat, callback);
+    //$.get("/query?q=song_subcat&arg=" + subcat, callback);
 }
 
 // get current track info
-export function currentTrack() {
+export function currentTrack(callback: any) {
     var callback = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : c;
 
-    $.get("/query?q=current", callback);
+    //$.get("/query?q=current", callback);
 }
 
 // get play queue
-export function playQueue() {
+export function playQueue(callback: any) {
     var callback = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : c;
-
-    $.get("/query?q=queue", callback);
+    //$.get("/query?q=queue", callback);
 }
 
 // get play history
-export function playHistory() {
+export function playHistory(callback: any) {
     var callback = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : c;
-
-    $.get("/query?q=history", callback);
+    //$.get("/query?q=history", callback);
 }
 
 // --- Event Functions ---
 
 // get all events
-export function getEvents() {
+export function getEvents(callback: any) {
     var callback = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : c;
-
-    $.get("/query?q=events", callback);
+    //$.get("/query?q=events", callback);
 }
 
 // get info on event
-export function getEvent(eventID) {
+export function getEvent(eventID: any, callback: any) {
     var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : c;
 
     if (eventID >= 0) {
-        $.get("/query?q=event_get&arg=" + eventID, callback);
+        //$.get("/query?q=event_get&arg=" + eventID, callback);
     } else {
-        error.log('Invalid Event ID');
+        console.error('Invalid Event ID');
     }
 }
 
 // delete event by ID
-export function deleteEvent(eventID) {
+export function deleteEvent(eventID: any, callback: any) {
     var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : c;
 
     if (eventID >= 0) {
-        $.get("/query?q=event_delete&arg=" + eventID, callback);
+        //$.get("/query?q=event_delete&arg=" + eventID, callback);
     } else {
         console.log('Invalid Event ID');
     }
 }
 
 // load event form
-export function loadEvent(id) {
+export function loadEvent(id: any, callback: any) {
     var copy = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
 
     var url = '/event-form';
@@ -135,43 +132,43 @@ export function loadEvent(id) {
             url = url + '&copy=' + copy;
         }
     }
-    window.location = url;
+    // redirect to form
 }
 
 // refesh events in radio DJ (nessisary to make changes to events take effect imediatly)
-export function refreshEvents() {
+export function refreshEvents(callback: any) {
     var callback = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : c;
 
-    sendControl('RefreshEvents', callback);
+    sendControl('RefreshEvents', '', callback);
 }
 
 // decode weekdays in repeat string
-export function decodeDays(string) {
+export function decodeDays(string: String, callback: any) {
     var codes = string.split('&');
     var date = '';
-    $.each(codes, function (i) {
-        if (codes[i] != '') {
-            if (i > 0) {
-                date += ", ";
-            }
-            date = date + moment(codes[i], "e").format("ddd");
-        }
-    });
+    // $.each(codes, function (i) {
+    //     if (codes[i] != '') {
+    //         if (i > 0) {
+    //             date += ", ";
+    //         }
+    //         date = date + codes[i];// moment(codes[i], "e").format("ddd");
+    //     }
+    // });
     return date;
 }
 
 // format date time info from radio DJ, into human comprehendable string
-export function formatDateTime(e) {
+export function formatDateTime(e: any, callback: any) {
     var dateTime = '';
     switch (e.type) {
         case 0:
-            dateTime = e.time + moment(e.date).format('dddd, MMMM Do YYYY');
+            dateTime = e.time + e.date;// moment(e.date).format('dddd, MMMM Do YYYY');
             break;
         case 1:
-            dateTime = e.time + decodeDays(e.day);
+            dateTime = e.time + decodeDays(e.day, c);
             break;
         case 2:
-            dateTime = 'Repeat by Hour' + decodeDays(e.day);
+            dateTime = 'Repeat by Hour' + decodeDays(e.day, c);
             break;
         case 3:
             dateTime = 'Manual Event';
@@ -181,7 +178,7 @@ export function formatDateTime(e) {
             break;
     }
     if (e.hours === '&') { } else {
-        time = e.hours;
+        dateTime = e.hours;
     }
     return dateTime;
 }
