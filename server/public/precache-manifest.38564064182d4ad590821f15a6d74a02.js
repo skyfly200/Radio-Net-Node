a0811ab9ea8b2d4b3ad8c11327c96f1f056575e1
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-06-04 10:46:25 -0600

    run build and remove dist in /client

---
+++ server/public/precache-manifest.38564064182d4ad590821f15a6d74a02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1ca5d372d0334e4d30d4",
+    "url": "/js/history.e11a8f75.js"
+  },
+  {
+    "revision": "6c23d97cb8f289dc864a",
+    "url": "/js/events.42e22efa.js"
+  },
+  {
+    "revision": "5f0de9527dbc2cf57eee",
+    "url": "/js/chunk-vendors.d8a85fac.js"
+  },
+  {
+    "revision": "9438591c46825a6f4fd0",
+    "url": "/js/app.43d30e2d.js"
+  },
+  {
+    "revision": "ff0aeafa4fc896cc785e5214eba6a603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f0de9527dbc2cf57eee",
+    "url": "/css/chunk-vendors.15ff7087.css"
+  },
+  {
+    "revision": "9438591c46825a6f4fd0",
+    "url": "/css/app.ceb7e67e.css"
+  }
+];
