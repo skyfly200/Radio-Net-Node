b5c126c5329f64fcd4f377f72c378f3d28087f11
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-06-20 06:09:50 -0600

    reorder ctrls + ormat types & times

--- client/src/components/Events.vue
+++ client/src/components/Events.vue
@@ -4,18 +4,15 @@
         h2 Events
         v-spacer
         .ctrls
-            CtrlBtn(:icon="eventsOn ? 'stop' : 'play_arrow'" @click='toggleEvents()' :tooltip="eventsOn ? 'Stop Events' : 'Start Events'")
             CtrlBtn(icon='add' @click='' tooltip='Add Event')
-            CtrlBtn(icon='update' @click='' tooltip='Refresh Events')
+            CtrlBtn(icon='update' @click='getEvents()' tooltip='Refresh Events')
+            CtrlBtn(:icon="eventsOn ? 'stop' : 'play_arrow'" @click='toggleEvents()' :tooltip="eventsOn ? 'Stop Events' : 'Start Events'")
     v-data-table(:headers="headers" :items="events" :total-items="events.length" hide-actions)
         template(v-slot:items="props")
             td {{ props.item.ID }}
             td {{ props.item.name }}
-            td {{ props.item.type }}
-            td {{ props.item.time }}
-            td {{ props.item.date }}
-            td {{ props.item.day }}
-            td {{ props.item.hours }}
+            td {{ types[props.item.type] }}
+            td {{ formatTime(props.item) }}
             td {{ props.item.catagory }}
             td.justify-center.layout.px-0
                 CtrlIcon.mr-2(icon='edit' @click='' tooltip='Edit Event')
@@ -24,40 +21,59 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
-import { setEvents } from "./lib/radio-dj";
+import format from "date-fns/format";
+import { setEvents, refreshEvents } from "./lib/radio-dj";
 import CtrlBtn from "./CtrlBtn.vue";
 import CtrlIcon from "./CtrlIcon.vue";
 
 @Component({
-    methods: { distanceInWordsToNow },
+    methods: { refreshEvents },
     components: { CtrlBtn, CtrlIcon }
 })
 export default class Events extends Vue {
     events = [];
-    eventsOn = false;
+    eventsOn = true;
+
+    types = ["One Time Only", "Repeat by Day", "Repeat by Day and Hour", "Manual Event", "Start-up Event", "Repeat by Date", "Repeat by Date and Hour", ];
+
     headers = [
         { text: 'ID', value: 'ID', align: 'left', sortable: false },
         { text: 'Name', value: 'name', sortable: false },
         { text: 'Type', value: 'type', sortable: false },
         { text: 'Time', value: 'time', sortable: false },
-        { text: 'Date', value: 'date', sortable: false },
-        { text: 'Day', value: 'day', sortable: false },
-        { text: 'Hours', value: 'hours', sortable: false },
         { text: 'Catagory', value: 'catagory', sortable: false }
     ];
 
     created() {
         // init now playing and playlist
         this.getEvents();
-        setInterval(function (this: any) {
-            (this as any).getEvents();
-            }.bind(this), 1000);
     }
 
     toggleEvents() {
         this.eventsOn = !this.eventsOn;
         setEvents(Number(this.eventsOn));
+    }
+
+    formatTime(item: any) {
+        switch(item.type) {
+            case 0: 
+                return `${item.time} ${format(item.date, 'MM/DD/YY')}`;
+                break;
+            case 1: 
+                return `${item.day.split('&').slice(1)}`;
+                break;
+            case 2: 
+                return `${item.day.split('&').slice(1)} ${item.hours.split('&').slice(1)}`;
+                break;
+            case 3: 
+                return "-";
+                break;
+            case 4: 
+                return "-";
+                break;
+            default:
+                return `${item.time} ${format(item.date, 'MM/DD/YY')}`;
+        }
     }
 
     getEvents() {
